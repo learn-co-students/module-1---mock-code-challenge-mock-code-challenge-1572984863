1c50df7095ce533536d6d5c3985f330efa4ac9bd
Griffin Poole <griffinpoole5@gmail.com>
2019-11-05 15:35:23 -0500

    Updated two classes Author and Book to meet deliverables given in readme. All methods tested and basic test suite settup in console.rb

--- author.rb
+++ author.rb
@@ -1,1 +1,34 @@
+class Author
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select{|book|
+            book.author == self
+        }
+    end
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end
+
+    def total_words
+        books.sum{|book|
+            book.word_count
+        }
+    end
+
+    def self.most_words
+        @@all.max_by{|author|
+            author.total_words
+        }
+    end
+end
