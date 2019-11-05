4baa1b757dd9366dd984d111c870902b392fa11a
ozubergz <jmin499@gmail.com>
2019-11-05 16:14:12 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,41 @@
+class Author
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select do |instance|
+            instance.author == self
+        end
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, word_count, self)
+    end
+
+    def total_words
+        books.map do |instance|
+            instance.word_count
+        end.sum
+    end
+
+    def self.most_words
+        max_num = self.all.map do |instance|
+            instance.total_words
+        end.max
+
+        self.all.find do |instance|
+            instance.total_words == max_num
+        end
+    end
+end
