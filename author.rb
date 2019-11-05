1bc764d6a9265ccfccdb9b78c82b6f332a5a00c3
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-05 15:50:27 -0500

    I think all these methods work now

--- author.rb
+++ author.rb
@@ -1,5 +1,31 @@
 class Author
 
+  attr_reader :name
+  @@all = []
 
+  def self.all
+    @@all
+  end
 
-end 
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def books
+    Book.all.select{|book| book.author == self}
+  end
+
+  def write_book (title, word_count)
+    Book.new(title, self, word_count)
+  end
+
+  def total_words
+    books.map{|book| book.word_count}.sum
+  end
+
+  def self.most_words
+    Author.all.max_by{|author| author.total_words}
+  end
+
+end
