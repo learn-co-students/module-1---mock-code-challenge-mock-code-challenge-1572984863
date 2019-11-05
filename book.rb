2422b3adc818fc642869aa561cbc4f0fce3fc9c2
github username <github email address>
2019-11-05 16:24:28 -0500

    Done

--- book.rb
+++ book.rb
@@ -1,1 +1,16 @@
+class Book
 
+    attr_reader :author, :books
+
+    @@all = []
+
+    def initialize(books)
+        @books = books
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,14 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+b1 = Book.new("The Great Gatsby")
+b2 = Book.new("Moby Dick")
+b3 = Book.new("War and Peace")
+
+a1 = Author.new("F S Fitzgerald")
+a2 = Author.new("Herman Melville")
+a3 = Author.new("Leo Tolstoy")
+
 binding.pry
 
 
