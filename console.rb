93a1a0af3c29a4d84330d1c17dceb11ac3bda27c
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-05 15:57:02 -0500

    Dominic's Mock

--- console.rb
+++ console.rb
@@ -2,6 +2,20 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+fitzgerald = Author.new
+lengle = Author.new
+nobokov = Author.new
+
+
+fitzgerald.write_book("The Great Gatsby", 10_000)
+lengle.write_book("A Wrinkle in Time", 5_000)
+nobokov.write_book("Lolita", 15_000)
+
+rain = Book.new(lengle, "The Small Rain", 6_000)
+ 
+
+
+
 binding.pry
 
-
+0
